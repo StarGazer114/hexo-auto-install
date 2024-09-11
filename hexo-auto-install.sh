353dab386e87c24eb770c3e0234661fba5c3fd4b
ljs <101713977+lijiashuai111@users.noreply.github.com>
2024-09-11 21:17:46 +0800

    Add files via upload

---
+++ hexo-auto-install.sh
@@ -0,0 +1,12 @@
+## 安装Node.js
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+## 安装hexo
+sudo npm install hexo-cli -g
+cd
+hexo init blog
+
+
+
+
